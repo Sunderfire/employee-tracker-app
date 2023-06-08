9feddf8113690cdd0cb481cd8b662129335ac56f
Griffin Pelletier <umbral.lights@live.com>
2023-06-08 14:54:29 -0400

    updated db

--- db/schema.sql
+++ db/schema.sql
@@ -14,5 +14,13 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
-)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT
+);
 
